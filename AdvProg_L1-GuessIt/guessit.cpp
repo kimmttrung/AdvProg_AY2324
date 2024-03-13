5a6618e6296091c2f5f1c57b70882e6cef26597d
Mai Trung <maitantrung23012005@gmail.com>
2024-03-13 10:18:24 +0700

    Test 2

--- AdvProg_L1-GuessIt/guessit.cpp
+++ AdvProg_L1-GuessIt/guessit.cpp
@@ -14,7 +14,7 @@ using namespace std;
 ***/
 int generateRandomNumber() {
     // TODO: Return the random number in range 1 to 100
-    return 100;
+    return rand() % 100 + 1 ;
 }
 
 
@@ -26,8 +26,9 @@ int generateRandomNumber() {
 ***/
 int getPlayerGuess() {
     // TODO: Ask the player guest and return the player's number
-
-    return 1;
+    int number;
+    cin >> number;
+    return number;
 }
 
 
@@ -46,7 +47,15 @@ string getAnswer(int number, int randomNumber) {
               If number is equal randomNumber, the answer is "Congratulation! You win."
     ***/
     string answer;
-
+    if (number == randomNumber) {
+    	answer = "Congratulation! You win.";
+		}
+    else if(number > randomNumber) {
+    	answer = "Your number is higher.";
+	} 
+    else if(number < randomNumber) {
+    	answer = "Your number is lower.";
+	}
     return answer;
 }
 
@@ -59,7 +68,12 @@ string getAnswer(int number, int randomNumber) {
 ***/
 bool checkSuccess(string answer) {
     // TODO: return the result after checking that player guessed right or wrong
-    
+    if (answer == "Your number is higher.") {
+        return false;
+    }
+    else if (answer == "Your number is lower.") {
+        return false;
+    }  
     return true;
 }
 
@@ -73,7 +87,10 @@ bool checkSuccess(string answer) {
 bool checkContinuePlaying(char isContinued) {
     // TODO: return result after checking player continue playing or not
     bool result = false;
-
+    
+    if (isContinued = 't'){
+    	return true;
+	}
     return result;
 }
 
@@ -87,7 +104,7 @@ bool checkContinuePlaying(char isContinued) {
 char getPlayerOpinion() {
     // TODO: Ask the player about continue playing and return the player's choice
     char isContinued;
-
+    cin >> isContinued;
     return isContinued;
 }
 
@@ -104,12 +121,13 @@ void playGuessIt() {
     } while (!checkSuccess(answer));
 }
 
-int run() {
+int main() {
     srand(time(0));
     char isContinued;
     do {
         playGuessIt();
         isContinued = getPlayerOpinion();
     } while (checkContinuePlaying(isContinued));
+
     return 0;
 }
