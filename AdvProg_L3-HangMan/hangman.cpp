61e099f540a24ae2e0bf59eeae00bf0286c5b74a
Mai Trung <maitantrung23012005@gmail.com>
2024-03-20 00:16:17 +0700

    Test 4

--- AdvProg_L3-HangMan/hangman.cpp
+++ AdvProg_L3-HangMan/hangman.cpp
@@ -55,10 +55,8 @@ bool isCharInWord(const char ch, const string& word)
     // TODO: return true if ch is in word else return false
     for(int i = 0;i < word.size(); i++) {
         if(ch == word[i]) return true;
-        else{
-            return false;
-        }
     }
+    return false;
 }
 
 /***
